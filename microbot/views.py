582c5c058384344e1921e5617f4f206b9f15ea04
MIKE <rlghd1996@gmail.com>
2017-10-26 13:53:20 +0900

    View 수정

--- microbot/views.py
+++ microbot/views.py
@@ -1,5 +1,5 @@
 import json
-import time
+import datetime
 from django.http import JsonResponse
 from Crawling.Crawl import Crawl
 from django.views.decorators.csrf import csrf_exempt
@@ -14,6 +14,6 @@ def crawl(request):
         obj = Crawl(mid)
         obj.main()
 
-        F_json = {'time': str(time.localtime()), 'api': 'item_detail', 'data': obj.data_list}
+        F_json = {'time': str(datetime.datetime.now()), 'api': 'item_detail', 'data': obj.data_list}
 
         return JsonResponse(F_json)
