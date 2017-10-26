c9b891334da7445c95e80f396d0b53499d874ffb
MIKE <rlghd1996@gmail.com>
2017-10-26 14:46:29 +0900

    Remove Djnago

---
+++ Crawling/Run.py
@@ -0,0 +1,23 @@
+from Crawling.Crawl import Crawl
+from selenium import webdriver
+from pyvirtualdisplay import Display
+from datetime import datetime
+
+
+def main():
+    display = Display(visible=0, size=(800, 600))
+    display.start()
+    driver = webdriver.Firefox(log_path='/home/pi/Django/geckodriver.log')
+    now = datetime.now()
+
+    print('start' + str(now))
+    obj = Crawl('5639964597', display, driver)
+    obj.main()
+    now = datetime.now()
+
+    F_json = {'time': now, 'api': 'item_detail', 'data': obj.data_list}
+    print('End'+ str(now))
+
+
+if __name__ == '__main__':
+    main()
